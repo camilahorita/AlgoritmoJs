3065a775fc4e75b953a46bd0290c897034f51477
Camila <camila@Dougs-MacBook-Air-5.local>
2022-04-29 18:36:44 +0200

    prompt alert and sum

---
+++ notes2.md
@@ -0,0 +1,14 @@
++ > soma quanto os tipos são numeros
++ > concatenação se uma das váriaveis for  string
+O dado que o prompt retorna é string não number
+
+camelCase
+Em ingles
+Sem  espaços e acentuações
+
+
+typeOf ( palavra reservada, é uma palavra que já existe dentro da linguagem e não podem ser utilizadas) > utilizado para descobrir o tipo de dado
+
+typeO variavel
+Number()
+bracket pair >color
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-/*
-  Encontre a solução ao problema
+/* 
+Solicite 2 números, faça a soma deles 
+e apresente o resultado final ao usuário
 
-  Pergunte o nome do usuário e escreva a mensagem:
-  "Olá, [nome do usuário]"
 */
-
-let nome = prompt ("Qual o seu nome?")
-alert("Olá, " + nome)
+alert("Vamos somar dois números");
+let numberOne = prompt(" Digite o primeiro número:");
+let numberTwo = prompt("Digite o segundo número:");
+let sum = Number(numberOne) + Number(numberTwo);
+alert("O resultado é " + sum);
