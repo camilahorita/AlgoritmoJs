cca395df7e5c34ba8f423d0aaa97f1a767218ab9
Camila <camila@Dougs-MacBook-Air-5.local>
2022-05-03 16:24:24 +0200

    fluxos de aplicacao/ boolean

--- notes.md
+++ notes.md
@@ -2,3 +2,18 @@ Fluxos de aplicação
 
 if/else (decidem qual caminho será tomado)
 não é possivel redeclarar uma variavel, para modificar seu valor você so inicia a variavel não redeclara com let var 
+
+> maior que
+> menor que
+>= maior ou igual 
+<= menor ou igual
+== igual
+!= diferente
+
+.toFixed() > quantidade de numeros depois da virgula >> funcionalidades para aplicar algum passo a passo
+
+if é uma palavra reservada para definir um fluxo de aplicação
+
+Boas praticas
+Não repetir codigo
+
--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 /* 
 Solicitar o nome do aluno e as 3 notas do bimestre
 calcular a media daquele aluno 
-A média deverá ser maior que 6
+A média deverá ser maior ou igual a 6
 Se o aluno passou de bimestre, dar os parabéns
 Se o aluno não passou de bimestre, motivar oaluno a dar o seu melhor na prova de recuperação
 Em ambos os casos, mostrar o nome do aluno e a nota
 */
 
-
-let name = prompt("Digite seu nome: ");
+let student = prompt("Digite seu nome: ");
 let gradeOne = Number(prompt("Digite sua primeira nota: "));
 let gradeTwo = Number(prompt("Digite sua segunda nota: "));
 let gradeThree = Number(prompt("Digite sua terceira nota: "));
 
-let finalGrade = (gradeOne + gradeTwo + gradeThree)/3;
+let finalGrade = (gradeOne + gradeTwo + gradeThree) / 3 ;
+finalGrade = finalGrade.toFixed(1);
 
 if (finalGrade >= 6) {
-  alert("Parabéns, " + name + " sua média é " + finalGrade + ". Você foi aprovado!");
+  alert("Parabéns, " + student + " sua nota final é " + finalGrade + ". Você foi aprovado." );
+} else if (finalGrade < 3) {
+  alert("Reprovado");
 } else {
-  alert( name + ", sua média é " + finalGrade + ". Se prepare para dar o seu melhor na recuperação, você consegue!")
+  alert(student + ", sua nota foi " + finalGrade + ". Estude mais para o exame.");
 }
