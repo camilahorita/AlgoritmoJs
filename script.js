f5bf81609be87c6c03f55586a67c19b5d2266523
Camila <camila@Dougs-MacBook-Air-5.local>
2022-05-04 14:24:07 +0200

    switch

--- script.js
+++ script.js
@@ -27,29 +27,61 @@ Variaveis
 */
 
 let option;
-let items = [];
-let n = 0;
+let list =[];
 
 while (option != 3) {
-option = Number(prompt(`Olá usuário, Digite o número da opção desejada:
 
-1. Cadastrar um item na lista
-2. Mostrar itens cadastrados
-3. Sair do programa`));
+  option = Number(prompt(`Olá usuário, Digite o número da opção desejada:
 
-if (option == 1) {
-  // items[n] =  prompt("Digite o nome do item");
-  // n++;
-  let item = prompt("Digite o nome do item");
-  items.push(item);
-}
-else if (option == 2) {
-  if(items.length==0){
-    alert("Não existe itens cadastrados");
-  }else {
-    alert(items);
+  1. Cadastrar um item na lista
+  2. Mostrar itens cadastrados
+  3. Sair do programa`));
+  switch(option){
+    case 1 : 
+      let item = prompt("Digite o nome do item");
+      list.push(item);
+      break;
+    case 2 :
+      if(list.length == 0) {
+        alert("Não existe itens cadastrados");
+      }else {
+        alert(list);
+      }
+      break;
+    case 3 :
+      alert("Tchau");
+      break;
+    default:
+      alert("Opção inválida, tente novamente")
   }
-}}
+}
 
-alert("Tchau")
+
+
+// let option;
+// let items = [];
+// let n = 0;
+
+// while (option != 3) {
+// option = Number(prompt(`Olá usuário, Digite o número da opção desejada:
+
+// 1. Cadastrar um item na lista
+// 2. Mostrar itens cadastrados
+// 3. Sair do programa`));
+
+// if (option == 1) {
+//   // items[n] =  prompt("Digite o nome do item");
+//   // n++;
+//   let item = prompt("Digite o nome do item");
+//   items.push(item);
+// }
+// else if (option == 2) {
+//   if(items.length==0){
+//     alert("Não existe itens cadastrados");
+//   }else {
+//     alert(items);
+//   }
+// }}
+
+// alert("Tchau")
 
